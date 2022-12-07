0558f595c3190b2483666f7b7d5ea1270194c0f6
smack0007 <zachary.snow@gmail.com>
2022-12-07 12:30:17 +0100

    Set viewport maximum-scale to 5.0.

--- src/+layout.tsx
+++ src/+layout.tsx
@@ -9,7 +9,7 @@ const layout: LayoutFunction = ({ content }: LayoutFunctionArgs) => {
           <meta charset="utf-8" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1"
+            content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
           />
           <meta name="author" content="Klaus Tappesser" />
           <meta
